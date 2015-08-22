abb72c3d865c3675fb5c88a5b6e03213220b98fc
Philip Gianfortoni <pwgianfortoni@gmail.com>
2015-08-22 19:28:56 -0400

    Add smooth moving.

--- scripts/game.js
+++ scripts/game.js
@@ -6,6 +6,7 @@ Game.init = function() {
   Game.keyboard = new Keyboard();
   Game.currentstage.addChild(Game.kraken.shape);
   Game.currentstage.update();
+  Game.speed = 1.0; // pixels per second
   createjs.Ticker.addEventListener("tick", Game.tick);
   $(window).keydown(Game.onKeydown);
 };
@@ -15,20 +16,24 @@ Game.tick = function(event) {
   Game.currentstage.update();
 };
 
+Game.timeToDist(vel, ms) {
+  return vel * ms * Game.speed * 0.001;
+};
+
 Game.onKeydown = function(event) {
   switch(event.which) {
-  	case Game.keyboard.LEFT:
-  		Game.kraken.moveLeft();
-  		break;
+    case Game.keyboard.LEFT:
+      Game.kraken.moveLeft();
+      break;
     case Game.keyboard.RIGHT:
-  		Game.kraken.moveRight();
-  		break;
-  	case Game.keyboard.UP:
-  		Game.kraken.moveUp();
-  		break;
+      Game.kraken.moveRight();
+      break;
+    case Game.keyboard.UP:
+      Game.kraken.moveUp();
+      break;
     case Game.keyboard.DOWN:
-  		Game.kraken.moveDown();
-  		break;
+      Game.kraken.moveDown();
+      break;
   }
 };
 
--- scripts/kraken.js
+++ scripts/kraken.js
@@ -1,31 +1,53 @@
 var Kraken = function() {
   var that = this;
-  this.x = 100;
-  this.y = 100;
+  this.x = 100.0;
+  this.y = 100.0;
   this.velX = 0;
   this.velY = 0;
+  this.speed = 5.0;
+  this.scale = 1.0;
   this.shape = this.createKrakenShape(this.x, this.y);
 };
 
 Kraken.prototype.update = function(event) {
+  this.x += Game.timeToDist(this.velX, event.delta); 
+  this.y += Game.timeToDist(this.velY, event.delta); 
   this.shape.x = this.x;
   this.shape.y = this.y;
+  this.shape.scaleX = this.scale;
+  this.shape.scaleY = this.scale;
 };
 
-Kraken.prototype.moveLeft = function() {
-  this.x = this.x - 5;
+Kraken.prototype.startLeft = function() {
+  this.velX -= this.speed * 1.0;
 };
 
-Kraken.prototype.moveRight = function() {
-  this.x = this.x + 5;
+Kraken.prototype.stopLeft = function() {
+  this.velX = 0.0;
 };
 
-Kraken.prototype.moveUp = function() {
-  this.y = this.y + 5;
+Kraken.prototype.startRight = function() {
+  this.velX += this.speed * 1.0;
 };
 
-Kraken.prototype.moveDown = function() {
-  this.y = this.y - 5;
+Kraken.prototype.stopRight = function() {
+  this.velX = 0.0;
+};
+
+Kraken.prototype.startUp = function() {
+  this.velY -= this.speed * 1.0;
+};
+
+Kraken.prototype.stopUp = function() {
+  this.velY = 0.0;
+};
+
+Kraken.prototype.startDown = function() {
+  this.velY += this.speed * 1.0;
+};
+
+Kraken.prototype.stopDown = function() {
+  this.velY = 0.0;
 };
 
 Kraken.prototype.createKrakenShape = function(x, y) {
