f3601da554ddb2c7fb270071b00738b7d2e08ed5
Elizabeth Kemp <kempy@kempy.org>
2015-08-23 16:48:54 -0700

    Add boat sprites

--- scripts/boat.js
+++ scripts/boat.js
@@ -1,6 +1,16 @@
+var BOAT_SPRITE_DATA = {
+    images: ['images/boat.png'],
+    frames: {width:821, height:499},
+    animations: {
+        sail:0
+    }
+  };
+
+var BOAT_SPRITE_SHEET = new createjs.SpriteSheet(BOAT_SPRITE_DATA);
+
 Boat = function(x) {
   this.x = x;
-  this.y = Game.water.waterLine - (0.2 * Game.water.waterLine);
+  this.y = Game.water.waterLine - (0.25 * Game.water.waterLine);
   this.shape = this.createBoatShape(this.x, this.y);
   this.brain = new RandomBrain(this, 0);
 };
@@ -12,16 +22,15 @@ Boat.prototype.preUpdate = function(event) {
 };
 
 Boat.prototype.createBoatShape = function(x, y) {
-  var boat = new createjs.Shape();
-  // Coordinates are the center (0,0)
-  boat.x = x;
-  boat.y = y;
-  // Draw circle from the center
-  boat.graphics.beginFill("BurlyWood").drawCircle(0, 0, 50);
-  boat.regX = 0;
-  boat.regY = 0;
-  // Bounds are relative to the center
-  boat.setBounds(-50, -50, 50, 50);
-
-  return boat;
-}
+  var sprite = new createjs.Sprite(BOAT_SPRITE_SHEET, 'sail');
+  var width = BOAT_SPRITE_DATA.frames.width;
+  var height = BOAT_SPRITE_DATA.frames.height;
+  sprite.setBounds(0, 0, width, height);
+  sprite.regX = width/2;
+  sprite.regY = height/2;
+  sprite.x = x;
+  sprite.y = y;
+  sprite.scaleX = 200 / width;
+  sprite.scaleY = sprite.scaleX;
+  return sprite;
+};
--- scripts/water.js
+++ scripts/water.js
@@ -1,18 +1,18 @@
 var Water = function() {
-  this.waterLine = 100;
+  this.waterLine = 200;
   this.shape = this.createShape();
 };
 
 Water.prototype.createShape = function() {
   var rectangle = new createjs.Shape();
-  rectangle.graphics.beginFill('LightBlue').drawRect(0, 100, Game.getWidth(), Game.getHeight());
+  rectangle.graphics.beginFill('LightBlue').drawRect(0, this.waterLine, Game.getWidth(), Game.getHeight());
   rectangle.regX = 0;
-  rectangle.regY = 100;
+  rectangle.regY = this.waterLine;
   rectangle.x = 0;
   rectangle.y = this.waterline;
   return rectangle;
 };
 
 Water.prototype.setSize = function() {
-  this.shape.graphics.drawRect(0, 100, Game.getWidth(), Game.getHeight());
+  this.shape.graphics.drawRect(0, this.waterLine, Game.getWidth(), Game.getHeight());
 };
